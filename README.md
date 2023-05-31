22a76ff9152c9e699b8a73ac6065c5bad82c0aaf
choieh <bobomi66@naver.com>
2023-05-31 14:24:10 +0900

    master upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# pf-gallery
+# postgal
+# 작성자 : 최은혜
+# 설명
+## 반응형 웹사이트
+git-pages : 
+
+### spec
+- HTML, CSS, jquery
+- 크로스브라우징 : Chrome, firefox, edge, safari
+- 미디어쿼리 : 모바일 우선(break point: 768px, 1023px, 1200px)
+- jqeury 슬라이드
+- fancybox jquery 라이브러리 사용
+
+### screen shot
+
+pc
+![pc](./img/pc.png)
+
+
+mobile
+<img src='./img/mo.png' width='40%'>
---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function (e) {
+
+
+    /*nav-bar*/
+    $(".nav-bar").click(function () {
+
+        if ($(".nav-bar").hasClass("clicked")) {
+            $("nav").fadeOut();
+            $(".nav-bar").removeClass("clicked");
+        } else {
+            $("nav").fadeIn();
+            $(".nav-bar").addClass("clicked");
+        }
+
+    });
+
+    $("nav li").click(function (event) {
+
+        $(this).addClass("select").siblings().removeClass("select");
+
+    });
+
+
+    /*chat - mobile*/
+    $(".chat-btn").click(function (e) {
+        e.preventDefault();
+        $(".chat-wrap").fadeIn();
+    });
+
+    $(".chat-top").click(function (e) {
+        e.preventDefault();
+        $(".chat-wrap").fadeOut();
+    });
+
+
+    /*chat - msg-sand*/
+    $("#chat-text").keydown(function (key) {
+
+        if (key.keyCode == 13) {
+
+            var text = $("#chat-text").val();
+
+            $("#chat-text").val('');
+
+            $(".chat-con").prepend("<div class='msg-send'>" + text + "</div>");
+
+        }
+
+    });
+});
